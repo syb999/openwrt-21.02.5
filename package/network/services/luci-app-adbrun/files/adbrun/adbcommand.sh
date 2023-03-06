b4fad887745582b8487fa7dc2e50e441b541ad30
syb999 <13062678135@163.com>
2023-03-06 21:34:49 +0800

    add services packages

---
+++ package/network/services/luci-app-adbrun/files/adbrun/adbcommand.sh
@@ -0,0 +1,219 @@
+#!/bin/sh
+
+spath="/usr/adbrun/"
+sectionname=$(echo $0 | cut -d '_' -f 2 | sed 's/^ADBRUN//')
+adbclient=$(uci get adbrun.$sectionname.adbiplist)
+adbcommand=$(uci get adbrun.$sectionname.adbcommandlist)
+
+adb connect ${adbclient}:5555
+screensize=$(adb -s ${adbclient}:5555 shell wm size | cut -d ':' -f 2 | sed -e "s/ //g;s/\n//g;s/\r//g")
+
+case $adbcommand in
+	turn-offon-the-screen) adbcd="shell input keyevent 26"
+	;;
+	turn-on-the-screen) adbcd="shell input keyevent 224"
+	;;
+	increase-screen-brightness) adbcd="shell input keyevent 221"
+	;;
+	reduce-screen-brightness) adbcd="shell input keyevent 220"
+	;;
+	playstop) adbcd="shell input keyevent 85"
+	;;
+	playnext) adbcd="shell input keyevent 87"
+	;;
+	playprevious) adbcd="shell input keyevent 88"
+	;;
+	resume-playback) adbcd="shell input keyevent 126"
+	;;
+	pause-playback) adbcd="shell input keyevent 127"
+	;;
+	mute) adbcd="shell input keyevent 164"
+	;;
+	runcamera) adbcd="shell am start -n com.android.camera/.Camera"
+	;;
+	photograph) adbcd="shell input keyevent 27"
+	;;
+	screenrecord) adbcd="shell screenrecord --time-limit 30 --size ${screensize} /data/local/tmp/screenrecord.mp4"
+	;;
+	dlscreenrecord) adbcd="pull /data/local/tmp/screenrecord.mp4 $(uci get adbrun.@adbinit[0].adbphotopath)/screenrecord.mp4"
+	;;
+	runwechat) adbcd="shell am start -n com.tencent.mm/.ui.LauncherUI"
+	;;
+	runqq) adbcd="shell am start -n com.tencent.mobileqq/.activity.SplashActivity"
+	;;
+	takephoto) adbcd="scripts"
+		adbsh="takephoto"
+	;;
+	screenshot) adbcd="scripts"
+		adbsh="screenshot"
+	;;
+	readbook) adbcd="scripts"
+		adbsh="readbook"
+	;;
+	kuaishou) adbcd="scripts"
+		adbsh="kuaishou"
+	;;
+	diantaolive) adbcd="scripts"
+		adbsh="diantaolive"
+	;;
+	autodiantao) adbcd="scripts"
+		adbsh="diantao"
+	;;
+	autojdlite) adbcd="scripts"
+		adbsh="jdlite"
+	;;
+	tbbbfarm) adbcd="scripts"
+		adbsh="tbbbfarm"
+	;;
+	none) adbcd=""
+	;;
+esac
+
+if [ $adbcd == "scripts" ];then
+	if [ ${adbsh} ==  "takephoto" ];then
+		cp ${spath}${adbsh} /tmp/ADBRUN${sectionname}_.sh
+		sed -i "s/starttime=.*/starttime=$(date +%s)/" /tmp/ADBRUN${sectionname}_.sh
+		chmod +x /tmp/ADBRUN${sectionname}_.sh
+		exec sh /tmp/ADBRUN${sectionname}_.sh
+	elif [ ${adbsh} ==  "screenshot" ];then
+		cp ${spath}${adbsh} /tmp/ADBRUN${sectionname}_.sh
+		sed -i "s/starttime=.*/starttime=$(date +%s)/" /tmp/ADBRUN${sectionname}_.sh
+		chmod +x /tmp/ADBRUN${sectionname}_.sh
+		exec sh /tmp/ADBRUN${sectionname}_.sh
+	elif [ ${adbsh} == "readbook" ];then
+		echo fanqiexiaoshuo
+		if [ ${screensize} == "720x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=680/;s/dosedbasey=/basey=130/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "720x1560" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=680/;s/dosedbasey=/basey=157/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "800x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=754/;s/dosedbasey=/basey=130/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2244" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=1030/;s/dosedbasey=/basey=300/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x1920" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=1030/;s/dosedbasey=/basey=200/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=1030/;s/dosedbasey=/basey=300/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2340" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=1030/;s/dosedbasey=/basey=395/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2400" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=1030/;s/dosedbasey=/basey=395/' > /tmp/ADBRUN${sectionname}_.sh
+		fi
+		sed -i "s/starttime=.*/starttime=$(date +%s)/" /tmp/ADBRUN${sectionname}_.sh
+		chmod +x /tmp/ADBRUN${sectionname}_.sh
+		exec sh /tmp/ADBRUN${sectionname}_.sh
+	elif [ ${adbsh} == "kuaishou" ];then
+		echo kuaishou
+		if [ ${screensize} == "720x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=386/;s/dosedbasey=/basey=620/;s/dosedbasecx=/basecx=655/;s/dosedbasecy=/basecy=145/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "720x1560" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=386/;s/dosedbasey=/basey=750/;s/dosedbasecx=/basecx=655/;s/dosedbasecy=/basecy=175/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "800x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=428/;s/dosedbasey=/basey=620/;s/dosedbasecx=/basecx=727/;s/dosedbasecy=/basecy=145/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2244" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=750/;s/dosedbasecx=/basecx=980/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x1920" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=700/;s/dosedbasecx=/basecx=780/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=750/;s/dosedbasecx=/basecx=980/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2340" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=845/;s/dosedbasecx=/basecx=1075/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2400" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=845/;s/dosedbasecx=/basecx=1075/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		fi
+		sed -i "s/starttime=.*/starttime=$(date +%s)/" /tmp/ADBRUN${sectionname}_.sh
+		chmod +x /tmp/ADBRUN${sectionname}_.sh
+		exec sh /tmp/ADBRUN${sectionname}_.sh
+	elif [ ${adbsh} == "diantaolive" ];then
+		echo diantaolive
+		if [ ${screensize} == "720x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=386/;s/dosedbasey=/basey=620/;s/dosedbasecx=/basecx=655/;s/dosedbasecy=/basecy=145/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "720x1560" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=386/;s/dosedbasey=/basey=750/;s/dosedbasecx=/basecx=655/;s/dosedbasecy=/basecy=175/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "800x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=428/;s/dosedbasey=/basey=620/;s/dosedbasecx=/basecx=727/;s/dosedbasecy=/basecy=145/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2244" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=750/;s/dosedbasecx=/basecx=980/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x1920" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=700/;s/dosedbasecx=/basecx=780/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=750/;s/dosedbasecx=/basecx=980/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2340" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=845/;s/dosedbasecx=/basecx=1075/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2400" ];then
+			cat ${spath}${adbsh} | sed 's/dosedbasex=/basex=586/;s/dosedbasey=/basey=845/;s/dosedbasecx=/basecx=1075/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		fi
+		sed -i "s/starttime=.*/starttime=$(date +%s)/" /tmp/ADBRUN${sectionname}_.sh
+		chmod +x /tmp/ADBRUN${sectionname}_.sh
+		exec sh /tmp/ADBRUN${sectionname}_.sh
+	elif [ ${adbsh} == "diantao" ];then
+		echo diantao
+		if [ ${screensize} == "720x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=360/;s/dosedystart=/ystart=515/;s/dosedbasex=/basex=605/;s/dosedbasey=/basey=586/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "720x1560" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=360/;s/dosedystart=/ystart=623/;s/dosedbasex=/basex=605/;s/dosedbasey=/basey=709/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "800x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=399/;s/dosedystart=/ystart=515/;s/dosedbasex=/basex=700/;s/dosedbasey=/basey=770/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2244" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=835/;s/dosedystart=/ystart=1050/;s/dosedbasex=/basex=910/;s/dosedbasey=/basey=1060/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x1920" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=835/;s/dosedystart=/ystart=850/;s/dosedbasex=/basex=910/;s/dosedbasey=/basey=860/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=835/;s/dosedystart=/ystart=1050/;s/dosedbasex=/basex=910/;s/dosedbasey=/basey=1060/;s/dosedbasecx=/basecx=980/;s/dosedbasecy=/basecy=220/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2340" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=835/;s/dosedystart=/ystart=1145/;s/dosedbasex=/basex=910/;s/dosedbasey=/basey=1155/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2400" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=835/;s/dosedystart=/ystart=1145/;s/dosedbasex=/basex=910/;s/dosedbasey=/basey=1350/' > /tmp/ADBRUN${sectionname}_.sh
+		fi
+		sed -i "s/starttime=.*/starttime=$(date +%s)/" /tmp/ADBRUN${sectionname}_.sh
+		chmod +x /tmp/ADBRUN${sectionname}_.sh
+		exec sh /tmp/ADBRUN${sectionname}_.sh
+	elif  [ ${adbsh} == "jdlite" ];then
+		echo jdlite
+		if [ ${screensize} == "720x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=350/;s/dosedystart=/ystart=800/;s/dosedbasex=/basex=620/;s/dosedbasey=/basey=550/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "720x1560" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=350/;s/dosedystart=/ystart=968/;s/dosedbasex=/basex=620/;s/dosedbasey=/basey=665/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "800x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=388/;s/dosedystart=/ystart=800/;s/dosedbasex=/basex=688/;s/dosedbasey=/basey=550/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2244" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=530/;s/dosedystart=/ystart=1560/;s/dosedbasex=/basex=950/;s/dosedbasey=/basey=835/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x1920" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=530/;s/dosedystart=/ystart=1360/;s/dosedbasex=/basex=950/;s/dosedbasey=/basey=635/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=530/;s/dosedystart=/ystart=1560/;s/dosedbasex=/basex=950/;s/dosedbasey=/basey=835/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2340" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=530/;s/dosedystart=/ystart=1655/;s/dosedbasex=/basex=950/;s/dosedbasey=/basey=930/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2400" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=530/;s/dosedystart=/ystart=1655/;s/dosedbasex=/basex=950/;s/dosedbasey=/basey=860/' > /tmp/ADBRUN${sectionname}_.sh
+		fi
+		sed -i "s/starttime=.*/starttime=$(date +%s)/" /tmp/ADBRUN${sectionname}_.sh
+		chmod +x /tmp/ADBRUN${sectionname}_.sh
+		exec sh /tmp/ADBRUN${sectionname}_.sh
+	elif [ ${adbsh} == "tbbbfarm" ];then
+		echo tbbbfarm
+		if [ ${screensize} == "720x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=300/;s/dosedystart=/ystart=1000/;s/dosedbasex=/basex=600/;s/dosedbasey=/basey=570/;s/dosedysetp1=/ysetp1=125/;s/dosedentbbx=/entbbx=500/;s/dosedentbby=/entbby=300/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "720x1560" ];then
+			echo "unsupport now"
+		elif [ ${screensize} == "768x1024" ];then
+			echo "unsupport now"
+		elif [ ${screensize} == "800x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=333/;s/dosedystart=/ystart=1000/;s/dosedbasex=/basex=666/;s/dosedbasey=/basey=570/;s/dosedysetp1=/ysetp1=125/;s/dosedentbbx=/entbbx=555/;s/dosedentbby=/entbby=300/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2244" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=300/;s/dosedystart=/ystart=1200/;s/dosedbasex=/basex=910/;s/dosedbasey=/basey=680/;s/dosedysetp1=/ysetp1=185/;s/dosedentbbx=/entbbx=550/;s/dosedentbby=/entbby=460/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x1920" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=300/;s/dosedystart=/ystart=1000/;s/dosedbasex=/basex=910/;s/dosedbasey=/basey=480/;s/dosedysetp1=/ysetp1=185/;s/dosedentbbx=/entbbx=550/;s/dosedentbby=/entbby=260/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=300/;s/dosedystart=/ystart=1200/;s/dosedbasex=/basex=910/;s/dosedbasey=/basey=680/;s/dosedysetp1=/ysetp1=185/;s/dosedentbbx=/entbbx=550/;s/dosedentbby=/entbby=460/' > /tmp/ADBRUN${sectionname}_.sh
+		elif [ ${screensize} == "1080x2340" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=300/;s/dosedystart=/ystart=1295/;s/dosedbasex=/basex=910/;s/dosedbasey=/basey=775/;s/dosedysetp1=/ysetp1=185/;s/dosedentbbx=/entbbx=550/;s/dosedentbby=/entbby=555/' > /tmp/ADBRUN${sectionname}_.sh
+		fi
+		sed -i "s/starttime=.*/starttime=$(date +%s)/" /tmp/ADBRUN${sectionname}_.sh
+		chmod +x /tmp/ADBRUN${sectionname}_.sh
+		exec sh /tmp/ADBRUN${sectionname}_.sh
+	fi
+else
+	adb -s ${adbclient}:5555 ${adbcd}
+fi
+
---
+++ package/network/services/luci-app-adbrun/luasrc/model/cbi/adbrun.lua
@@ -0,0 +1,134 @@
+m = Map("adbrun", translate("ADB server"))
+
+m:section(SimpleSection).template  = "adbrun_status"
+
+a = m:section(TypedSection, "adbinit", "")
+
+a:tab("adb_init", translate("init devices"))
+adbinit = a:taboption("adb_init", Button, "_adbinit", translate("One-click initialize devices"))
+adbinit.rmempty = true
+adbinit.inputstyle = "apply"
+adbinit.description = translate("Please use USB cable to connect openwrt router and Android device(multi devices support)")
+function adbinit.write(self, section)
+	luci.util.exec("/usr/adbrun/adbinit.sh >/dev/null 2>&1 &")
+end
+
+adbstophotplug = a:taboption("adb_init", Button, "adbstophotplug", translate("One-click disable hotplug"))
+adbstophotplug.rmempty = true
+adbstophotplug.inputstyle = "apply"
+adbstophotplug.description = translate("Suitable for connecting to openwrt through USB cable for a long time")
+function adbstophotplug.write(self, section)
+	luci.util.exec("mv /etc/hotplug.d/usb/30-adb_init /etc/30-adb_init >/dev/null 2>&1 &")
+end
+
+adbxhotplug = a:taboption("adb_init", Button, "adbxhotplug", translate("One-click resume hotplug"))
+adbxhotplug.rmempty = true
+adbxhotplug.inputstyle = "apply"
+adbxhotplug.description = translate("After hot plug is enabled, you can continue to initialize the device using USB cable")
+function adbxhotplug.write(self, section)
+	luci.util.exec("mv /etc/30-adb_init /etc/hotplug.d/usb/30-adb_init >/dev/null 2>&1 &")
+end
+
+a:tab("diantaoinit_set", translate("diantao init setting"))
+diantaoskip = a:taboption("diantaoinit_set", Value, "diantaoskip", translate("diantao skip step(default for 1080x1920)"))
+diantaoskip.datatype = "string"
+diantaoskip.default = "210"
+diantaoskip.rmempty = false
+
+
+diantaodayworklist = a:taboption("diantaoinit_set", Value, "diantaodayworklist", translate("diantao daytime worklist"))
+diantaodayworklist.datatype = "string"
+diantaodayworklist.default = "skip 60秒2次 skip 3分钟1次 5分钟2次 5分钟3次 60秒1次"
+diantaodayworklist.rmempty = false
+
+diantaonightworklist=a:taboption("diantaoinit_set", Value, "diantaonightworklist", translate("diantao night worklist"))
+diantaonightworklist.datatype = "string"
+diantaonightworklist.default = "30秒20次 60秒20次 60秒3次 3分钟1次 30秒1次 30秒1次 5分钟1次 8分钟1次 30秒1次"
+diantaonightworklist.rmempty = false
+
+a:tab("photoinit_set", translate("photo init setting"))
+adbphotopath = a:taboption("photoinit_set", Value, "adbphotopath", translate("Photos directory"))
+adbphotopath.datatype = "string"
+adbphotopath.default = "/tmp"
+adbphotopath.rmempty = false
+adbphotopath.description = translate("Please enter a valid directory")
+
+s = m:section(TypedSection, "adbrun", "", translate("Assistant for automatic control android devices."))
+
+s:tab("adb_set", translate("Basic setting"))
+
+s.anonymous = false
+s.addremove = true
+
+adbiplist = s:taboption("adb_set",Value, "adbiplist", translate("IP address")) 
+adbiplist.rmempty = true
+adbiplist.datatype = "ipaddr"
+luci.sys.net.ipv4_hints(function(ip, name)
+	adbiplist:value(ip, "%s (%s)" %{ ip, name })
+end)
+
+adbcommandlist = s:taboption("adb_set", ListValue, "adbcommandlist", translate("Command list"), translate("adbrun command list"))
+adbcommandlist.placeholder = "none"
+adbcommandlist:value("none", translate("none"))
+adbcommandlist:value("turn-offon-the-screen", translate("Turn off/on the screen"))
+adbcommandlist:value("turn-on-the-screen", translate("Turn on screen"))
+adbcommandlist:value("increase-screen-brightness", translate("Increase screen brightness"))
+adbcommandlist:value("reduce-screen-brightness", translate("Reduce screen brightness"))
+adbcommandlist:value("playstop", translate("Play or stop"))
+adbcommandlist:value("playnext", translate("Play the next"))
+adbcommandlist:value("playprevious", translate("Play the previous"))
+adbcommandlist:value("resume-playback", translate("Resume playback"))
+adbcommandlist:value("pause-playback", translate("Pause playback"))
+adbcommandlist:value("mute", translate("Mute on/off"))
+adbcommandlist:value("runcamera", translate("Run camera"))
+adbcommandlist:value("photograph", translate("Take a picture"))
+adbcommandlist:value("screenrecord", translate("Screen record 30s"))
+adbcommandlist:value("dlscreenrecord", translate("Push screen record to openwrt"))
+adbcommandlist:value("runwechat", translate("Run Wechat"))
+adbcommandlist:value("runqq", translate("Run QQ"))
+adbcommandlist:value("takephoto", translate("Automatically take photos"))
+adbcommandlist:value("screenshot", translate("Automatically push screenshot to openwrt"))
+adbcommandlist:value("readbook", translate("Automatically read book"))
+adbcommandlist:value("kuaishou", translate("Automatically play kuaishou"))
+adbcommandlist:value("diantaolive", translate("Automatically diantaolive"))
+adbcommandlist:value("autodiantao", translate("Automatically taobao live"))
+adbcommandlist:value("autojdlite", translate("Automatically jdlite"))
+adbcommandlist:value("tbbbfarm", translate("Automatically taobao baba farm"))
+adbcommandlist.default     = "none"
+adbcommandlist.rempty      = false
+
+
+s:tab("adb_action", translate("Action"))
+
+adbdisconnect = s:taboption("adb_action",Button,"adbdisconnect",translate("Disconnect the current client"))
+adbdisconnect.rmempty = true
+adbdisconnect.inputstyle = "apply"
+function adbdisconnect.write(self, section)
+	luci.util.exec("adb disconnect $(uci get adbrun."..section..".adbiplist) >/dev/null 2>&1 &")
+end
+
+adbconnect = s:taboption("adb_action",Button,"adbconnect",translate("Connect the current client"))
+adbconnect.rmempty = true
+adbconnect.inputstyle = "apply"
+function adbconnect.write(self, section)
+	luci.util.exec("adb connect $(uci get adbrun."..section..".adbiplist) >/dev/null 2>&1 &")
+end
+
+adbplay = s:taboption("adb_action",Button, "adbplay", translate("Play")) 
+adbplay.rmempty = true
+adbplay.inputstyle = "apply"
+function adbplay.write(self, section)
+	luci.util.exec("cp /usr/adbrun/adbcommand.sh /tmp/adb_ADBRUN" ..section.. "_.sh >/dev/null 2>&1 &")
+	luci.util.exec("sleep 1")
+	luci.util.exec("/tmp/adb_ADBRUN" ..section.. "_.sh >/dev/null 2>&1 &")
+end
+
+adbstop = s:taboption("adb_action",Button, "adbstop", translate("Stop loop script")) 
+adbstop.rmempty = true
+adbstop.inputstyle = "apply"
+function adbstop.write(self, section)
+	luci.util.exec("kill -9 $(busybox ps | grep ADBRUN" ..section.. "_ | grep -v grep | awk '{print$1}') > /dev/null 2>&1")
+end
+
+return m
+
