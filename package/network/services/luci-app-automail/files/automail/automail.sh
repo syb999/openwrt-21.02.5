33e374eb0e74db7148ed59a5715e96a04f492763
syb999 <13062678135@163.com>
2023-03-07 20:20:42 +0800

    luci-app-automail: update

--- package/network/services/luci-app-automail/files/automail/automail.sh
+++ package/network/services/luci-app-automail/files/automail/automail.sh
@@ -7,7 +7,7 @@ target=$(uci get automail.@automail[0].recipient)
 #默认发送测试邮件内容，请自行修改
 echo -e "邮件发送时间:$(date +%Y-%m-%d\ %H:%M)\n你好，我是一封测试邮件！" | mutt -s "来自AutoMail的邮件" -- $target
 
+rm /root/sent >/dev/null 2>&1
 
-echo "clear" > /root/sent
 
 
